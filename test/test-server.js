14caafa07a42f1cc9d5e007ba3073634c471b9d5
Redrogue12 <exgc12@gmail.com>
2017-06-01 00:43:33 -0400

    Added GET and POST test for /recipes

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,48 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+
+    before(function() {
+        return runServer();
+    })
+
+    after(function() {
+        return closeServer();
+    })
+
+    //GET test
+    it('should list items on GET', function() {
+        return chai.request(app)
+        .get('/recipes')
+        .then(function(res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('array');
+            res.body.length.should.be.at.least(1);
+
+            const expectedKeys = ['id', 'name', 'ingredients'];
+            res.body.forEach(function(item) {
+                item.should.be.a('object');
+                item.should.include.keys(expectedKeys);
+            });
+        });
+    });
+
+    it('should add an item on POST', function() {
+        const newItem = {name: 'recipeName', ingredients: ['one', 'two', 'three']};
+        return chai.request(app)
+            .post('/recipes')
+            .send(newItem)
+            .then(function(res) {
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.include.keys('id', 'name', 'ingredients');
+                res.body.id.should.not.be.null;
+                res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+            });
+    });
+
+}); //end of Recipes test
